b7a3052bfc6376e1d6353a5294b7c7c243e6e5cd
ctaaag <qudxor102@gmail.com>
2022-11-16 18:56:25 +0900

    start

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="home">
+		<div>좌충우돌 vue 학습기</div>
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+	name: "HomeView",
+	// components: {
+	//   HelloWorld
+	// }
+};
+</script>

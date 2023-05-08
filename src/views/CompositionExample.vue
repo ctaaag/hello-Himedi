7db1998f1ec9a4996cfb96c06940e70274ed8f97
ctaaag <qudxor102@gmail.com>
2023-05-08 19:41:30 +0900

    fix: 메인페이지 수정

--- src/views/CompositionExample.vue
+++ src/views/CompositionExample.vue
@@ -1,35 +1,35 @@
 <template lang="">
-	<div>count is:{{ state.count }}, Double is {{ state.double }}</div>
-	<button @click="increment">increment</button>
-	<div class="mixin_component">
-		<div>mixin 예제 : {{ sample }}</div>
-		<button @click="onClick">mixinButton</button>
-	</div>
-	<!-- 일반 composition 사용 -->
-	<div class="composition_exam2">
-		<span>composition setup state 선언 : </span>
-		<input type="number" v-model="state.numExam1" />
-		<span>+</span>
-		<input type="number" v-model="state.numExam2" />
-		<span>=</span>
-		<span>{{ state.result }}</span>
-	</div>
-	<div>
-		<span>composition 컴포넌트 내부 선언 : </span>
-		<input type="number" v-model="num1" />
-		<span>+</span>
-		<input type="number" v-model="num2" />
-		<span>=</span>
-		<span>{{ result }}</span>
-	</div>
-	<div>
-		<span>composition 컴포넌트 외부 선언 : </span>
-		<input type="number" v-model="numbers.number1" />
-		<span>+</span>
-		<input type="number" v-model="numbers.number2" />
-		<span>=</span>
-		<span>{{ numbers.sampleResult }}</span>
-	</div>
+  <div>count is:{{ state.count }}, Double is {{ state.double }}</div>
+  <button @click="increment">increment</button>
+  <div class="mixin_component">
+    <div>mixin 예제 : {{ sample }}</div>
+    <button @click="onClick">mixinButton</button>
+  </div>
+  <!-- 일반 composition 사용 -->
+  <div class="composition_exam2">
+    <span>composition setup state 선언 : </span>
+    <input type="number" v-model="state.numExam1" />
+    <span>+</span>
+    <input type="number" v-model="state.numExam2" />
+    <span>=</span>
+    <span>{{ state.result }}</span>
+  </div>
+  <div>
+    <span>composition 컴포넌트 내부 선언 : </span>
+    <input type="number" v-model="num1" />
+    <span>+</span>
+    <input type="number" v-model="num2" />
+    <span>=</span>
+    <span>{{ result }}</span>
+  </div>
+  <div>
+    <span>composition 컴포넌트 외부 선언 : </span>
+    <input type="number" v-model="numbers.number1" />
+    <span>+</span>
+    <input type="number" v-model="numbers.number2" />
+    <span>=</span>
+    <span>{{ numbers.sampleResult }}</span>
+  </div>
 </template>
 <script>
 import { reactive, computed, ref } from "vue";
@@ -37,43 +37,43 @@ import mixinSample from "../components/mixin/mixinSample";
 import { compositionReactiveSample } from "../components/composition/compositionExample";
 
 function plusCalculator() {
-	const num1 = ref(0);
-	const num2 = ref(0);
-	const result = ref(computed(() => num1.value + num2.value));
+  const num1 = ref(0);
+  const num2 = ref(0);
+  const result = ref(computed(() => num1.value + num2.value));
 
-	return { num1, num2, result };
+  return { num1, num2, result };
 }
 
 export default {
-	setup() {
-		let { numbers } = compositionReactiveSample();
+  setup() {
+    let { numbers } = compositionReactiveSample();
 
-		let { num1, num2, result } = plusCalculator();
-		const state = reactive({
-			numExam1: 0,
-			numExam2: 0,
-			count: 0,
-			double: computed(() => state.count * 2),
-			result: computed(() => state.numExam1 + state.numExam2),
-		});
-		const increment = () => state.count++;
-		return {
-			state,
-			increment,
-			num1,
-			num2,
-			result,
-			numbers,
-		};
-	},
-	mixins: [mixinSample],
+    let { num1, num2, result } = plusCalculator();
+    const state = reactive({
+      numExam1: 0,
+      numExam2: 0,
+      count: 0,
+      double: computed(() => state.count * 2),
+      result: computed(() => state.numExam1 + state.numExam2),
+    });
+    const increment = () => state.count++;
+    return {
+      state,
+      increment,
+      num1,
+      num2,
+      result,
+      numbers,
+    };
+  },
+  mixins: [mixinSample],
 };
 </script>
 <style scoped>
 .composition_exam2 {
-	margin-top: 30px;
+  margin-top: 30px;
 }
 .mixin_component {
-	margin-top: 30px;
+  margin-top: 30px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,63 @@
 <template>
-	<div class="home" style="text-align: center">
-		<div>좌충우돌 Vue.js 온보딩</div>
-		<img alt="Vue logo" src="../assets/images/logo.png" />
-		<nav>
-			<router-link to="/">Home</router-link>
-			<router-link to="/datetime">DateTime</router-link>
-			<router-link to="/i18n">I18n</router-link>
-			<router-link to="/l10n">L10n</router-link>
-			<router-link to="/rtl">RTL</router-link>
-			<router-link to="/axios">Axios & Fetch</router-link>
-			<router-link to="/router">Router & LifeCycle</router-link>
-			<router-link to="/composition">Composition</router-link>
-			<!-- <router-link to="{name:'todoapp'}">Todo(composition)</router-link> -->
-			<router-link to="/todoapp">Todo(composition)</router-link>
-			<router-link to="/sassapple">SassApple</router-link>
-			<router-link to="/webfont">WebFont</router-link>
-		</nav>
-	</div>
+  <div class="home" style="text-align: center">
+    <h1 class="main_title">병택의 HIMEDI 온보딩 저장소</h1>
+    <img
+      class="main_image"
+      alt="Main Image"
+      src="../assets/images/onboarding-image.png"
+    />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/datetime">DateTime</router-link>
+      <router-link to="/i18n">I18n</router-link>
+      <router-link to="/l10n">L10n</router-link>
+      <router-link to="/rtl">RTL</router-link>
+      <router-link to="/axios">Axios & Fetch</router-link>
+      <router-link to="/router">Router & LifeCycle</router-link>
+      <router-link to="/composition">Composition</router-link>
+      <!-- <router-link to="{name:'todoapp'}">Todo(composition)</router-link> -->
+      <router-link to="/todoapp">Todo(composition)</router-link>
+      <router-link to="/sassapple">SassApple</router-link>
+      <router-link to="/webfont">WebFont</router-link>
+    </nav>
+    <a class="blog_link" href="https://ctaaaglog.vercel.app/" target="_blank"
+      >구체적인 이야기가 궁금하다면 Blog로 오세요!</a
+    >
+  </div>
 </template>
 
 <script>
 export default {
-	name: "HomeView",
+  name: 'HomeView',
 };
 </script>
 
 <style>
 nav {
-	padding: 30px;
-	text-align: center;
+  padding: 30px;
+  text-align: center;
 }
 
 nav a {
-	font-weight: bold;
-	color: #2c3e50;
-	margin: 15px;
+  font-weight: bold;
+  color: #2c3e50;
+  margin: 15px;
 }
 
 nav a.router-link-exact-active {
-	color: #42b983;
+  color: #42b983;
+}
+
+.main_image {
+  width: 60%;
+}
+
+.main_title {
+  margin: 20px;
+}
+
+.blog_link {
+  text-decoration: none;
+  color: black;
 }
 </style>
